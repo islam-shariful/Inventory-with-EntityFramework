098376463469b0a6cd20af2f931a61dc5f54afa5
Md. Shariful Islam <imdshariful171@gmail.com>
2020-11-09 03:50:43 +0600

    View+Create

---
+++ Inventory with EntityFramework/Controllers/CategoryController.cs
@@ -0,0 +1,38 @@
+﻿using Inventory_with_EntityFramework.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Inventory_with_EntityFramework.Controllers
+{
+    public class CategoryController : Controller
+    {
+        InventoryDB context = new InventoryDB();
+        // GET: Category
+        public ActionResult Index()
+        {
+            ////LINQ => Lnaguage Integrated Query
+            //var categoryList = from item in context.Categories
+            //                   select item;
+            //return View(categoryList.ToList());
+
+            //Lamda Expression
+            return View(context.Categories.ToList());
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Category cat)
+        {
+            context.Categories.Add(cat);
+            context.SaveChanges();
+            return RedirectToAction("/Index");
+        }
+    }
+}
